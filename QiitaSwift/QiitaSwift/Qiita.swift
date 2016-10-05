d08789bcd1cbab10b0c4656a1853d45d600cab05
Takeo Asai <spitson+github@gmail.com>
2016-10-05 17:31:19 +0900

    init

---
+++ QiitaSwift/QiitaSwift/Qiita.swift
@@ -0,0 +1,15 @@
+//
+//  Qiita.swift
+//  QiitaSwift
+//
+//  Created by 浅井岳大 on 2016/10/05.
+//  Copyright © 2016年 浅井 岳大. All rights reserved.
+//
+
+import Foundation
+
+public class Qiita {
+    public init() {
+        print("test")
+    }
+}
---
+++ QiitaSwift/QiitaSwift/QiitaSwift.h
@@ -0,0 +1,19 @@
+//
+//  QiitaSwift.h
+//  QiitaSwift
+//
+//  Created by 浅井岳大 on 2016/10/05.
+//  Copyright © 2016年 浅井 岳大. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QiitaSwift.
+FOUNDATION_EXPORT double QiitaSwiftVersionNumber;
+
+//! Project version string for QiitaSwift.
+FOUNDATION_EXPORT const unsigned char QiitaSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QiitaSwift/PublicHeader.h>
+
+
